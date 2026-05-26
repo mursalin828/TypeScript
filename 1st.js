0c65a6defc646350c28288fdbad858268a64b100
mursalin828 <mursalingm828@gmail.com>
2026-05-26 16:05:45 +0600

    first day

---
+++ 1st.js
@@ -0,0 +1,8 @@
+"use strict";
+let isEcoFriendly = true;
+let totalPages = 180;
+let brandTagLine = "Making a new world";
+console.log(totalPages);
+totalPages = "one hunded";
+console.log(totalPages);
+isEcoFriendly = false;
---
+++ 1st.ts
@@ -0,0 +1,14 @@
+let isEcoFriendly: boolean = true;
+
+let totalPages: number = 180;
+
+let brandTagLine: string = "Making a new world";
+
+console.log(totalPages);
+
+
+totalPages = 190;
+
+console.log(totalPages);
+
+isEcoFriendly = false;
